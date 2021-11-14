e4c06af32fb55ab6d738fd078d5193b9832f33be
Javier Pagan <9905163+javierjpagan@users.noreply.github.com>
2021-11-13 22:28:06 -0500

    Adding code to connect to Rinkeby network

--- deploy.js
+++ deploy.js
@@ -1,1 +1,9 @@
-// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode} = require('.compile');
+
+const provider = new HDWalletProvider(
+    // *Account mnemonic*,
+    'https://rinkeby.infura.io/v3/b74b7e8c46f8498482f071001a4239e0'
+);
+const web3 = new Web3(provider);
