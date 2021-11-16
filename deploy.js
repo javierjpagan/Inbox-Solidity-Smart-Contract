35480068d732bb82aa67e7faa522546ef7da321e
Javier Pagan <9905163+javierjpagan@users.noreply.github.com>
2021-11-16 18:37:04 -0500

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -10,6 +10,15 @@ const web3 = new Web3(provider);
 
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
-    
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+   const result = await new web3.eth.Contract(JSON.parte(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] })
+
+    console.log('Contract deployted to', result.options.address);
+    provider.engine.stop();
+
 };
 deploy();
